6247f000d4c2e99592b41d953239c902509d90ff
tfrost56 <tfrost56@gmail.com>
2020-06-15 17:07:14 +0100

    javascipt animation

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ const nav = document.querySelector(".navigation");
 menuBTN.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
 });
+
+
+var text = document.querySelector(".intro__sub-header");
+var textContent = text.getElementsByTagName("span");
+
+var i = 0;
+
+function rotator () {
+    textContent[i].style.display = 'none';
+    i = (i + 1) % textContent.length; 
+    textContent[i].style.display = 'initial';
+}
+setInterval (rotator, 2000);
